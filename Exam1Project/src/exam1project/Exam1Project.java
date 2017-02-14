da3e6f6a465bd83a24d2d9fb3ed45cb2501ffc03
Jeremy Schaefer <Jeremy Schaefer>
2017-02-14 14:40:47 -0600

    Completed Exam 1 Takehome portion.

--- Exam1Project/src/exam1project/Exam1Project.java
+++ Exam1Project/src/exam1project/Exam1Project.java
@@ -8,6 +8,7 @@ package exam1project;
  
 import jsjf.ArrayStack;
 import jsjf.LinkedStack;
+import jsjf.exceptions.*;
 
 /**
  *
@@ -20,45 +21,82 @@ public class Exam1Project {
      */
     public static void main(String[] args) {
         
-	// 
 	// TODO EXAM 1 - TAKEHOME
 	// 1. Complete the ArrayStack and LinkedStack classes (all 6 methods)
         // 2. In this main, write a test for each method AND any exceptions
         // 3. Push all of the changes back to the repository (git push)
-
-
-        ArrayStack myArrayStack = new ArrayStack();
         
-        /* 
-        //Debugging sequence
-        
+        arrayStackTest();
+        linkedStackTest();
+    }
+    
+    private static void arrayStackTest()
+    {
+        ArrayStack<String> arrayStack = new ArrayStack<>();
         System.out.println("ArrayStack test...");
-        ArrayStack<String> arrayStack = new ArrayStack<String>();
-        System.out.println("Is empty? " + arrayStack.isEmpty());
+        
+        //Test push().
         arrayStack.push("Element 1");
         arrayStack.push("Element 2");
         arrayStack.push("Element 3");
-        System.out.println(arrayStack.pop());
-        System.out.println(arrayStack.peek());
+        
+        //Test pop() and peek().
+        System.out.println("Pop: " + arrayStack.pop());
+        System.out.println("Peek: " + arrayStack.peek());
+        
+        //Test isEmpty() and toString().
         System.out.println("Is empty? " + arrayStack.isEmpty());
         System.out.println("toString(): " + arrayStack.toString());
-        arrayStack.pop();
-        arrayStack.pop();
-        System.out.println("Is empty? " + arrayStack.isEmpty());
         
+        //Pop everything out of the stack.
+        arrayStack.pop();
+        arrayStack.pop();
+        
+        //Attempt to pop while the stack is empty and
+        //then handle the exception.
+        try {
+            arrayStack.pop();
+        }
+        catch(EmptyCollectionException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        //Check that the stack is actually empty.
+        System.out.println("Is empty? " + arrayStack.isEmpty());
+    }
+    
+    private static void linkedStackTest()
+    {
+        LinkedStack<String> linkedStack = new LinkedStack<>();
         System.out.println("\nLinkedStack test...");
-        LinkedStack<String> linkedStack = new LinkedStack<String>();
-        System.out.println("Is empty? " + linkedStack.isEmpty());
+        
+        //Test push().
         linkedStack.push("Element 1");
         linkedStack.push("Element 2");
         linkedStack.push("Element 3");
-        System.out.println(linkedStack.pop());
-        System.out.println(linkedStack.peek());
+        
+        //Test pop() and peek().
+        System.out.println("Pop: " + linkedStack.pop());
+        System.out.println("Peek: " + linkedStack.peek());
+        
+        //Test isEmpty() and toString().
         System.out.println("Is empty? " + linkedStack.isEmpty());
         System.out.println("toString(): " + linkedStack.toString());
+        
+        //Pop everything out of the stack.
         linkedStack.pop();
         linkedStack.pop();
+        
+        //Attempt to pop while the stack is empty and
+        //then handle the exception.
+        try {
+            linkedStack.pop();
+        }
+        catch(EmptyCollectionException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        //Check that the stack is actually empty.
         System.out.println("Is empty? " + linkedStack.isEmpty());
-        */
     }
 }
